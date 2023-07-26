0e9bb85b99b7d68a2099e9b474e0aedfff345bbc
tiodot <tiodot@163.com>
2023-07-26 20:40:57 +0800

    倒计时支持暂停

--- src-tauri/inject/countdown.js
+++ src-tauri/inject/countdown.js
@@ -3,6 +3,7 @@ function countdown(dom, total = 15) {
   const countdownNumberEl = dom.querySelector('.ti-countdown-number');
   const countdownCircleEl = dom.querySelector('.ti-countdown-circle');
   const initialOffset = 138.2;
+  let status = 'playing'; // done, pause
 
   if (!countdownCircleEl || !countdownNumberEl) {
     countdown.retry <= 3 && setTimeout(() => {
@@ -13,12 +14,13 @@ function countdown(dom, total = 15) {
 
   // 设置倒计时开始时间和结束时间
   let startTime = Date.now();
-  const endTime = startTime + total * 60 * 1000;
+  let endTime = startTime + total * 60 * 1000;
+  let remainingTime = endTime - startTime;
 
   // 更新倒计时数字和圆环的状态
   function updateCountdown() {
     const now = Date.now();
-    const remainingTime = endTime - now;
+    remainingTime = endTime - now;
     const minutes = Math.ceil(remainingTime / 1000 / 60);
 
     // 更新倒计时数字
@@ -34,36 +36,65 @@ function countdown(dom, total = 15) {
       clearInterval(countdownInterval);
       countdownNumberEl.textContent = "✔";
       countdownCircleEl.style.strokeDashoffset = 0;
+      status = 'done';
     }
   }
 
   updateCountdown();
   // 每秒更新一次倒计时状态
-  const countdownInterval = setInterval(updateCountdown, 1000);
-  return () => {
-    clearInterval(countdownInterval);
-    countdownCircleEl.style.strokeDashoffset = -0.1; 
-  };
+  let countdownInterval = setInterval(updateCountdown, 1000);
+
+  const actions = {
+    reset() {
+      clearInterval(countdownInterval);
+      countdownCircleEl.style.strokeDashoffset = -0.1;
+    },
+    pause() {
+      clearInterval(countdownInterval);
+      countdownNumberEl.textContent = '⏵';
+      status = 'pause';
+    },
+    resume() {
+      if (status === 'pause') {
+        // 需要重新更新一个时间，避免计算出现问题
+        endTime = remainingTime + Date.now();
+
+        // 更新倒计时数字
+        countdownNumberEl.textContent = Math.ceil(remainingTime / 1000 / 60);
+        countdownInterval = setInterval(updateCountdown, 1000);
+      }
+      status = 'playing';
+    },
+    toggle() {
+      if (status === 'done') {
+        return countdown(dom, total);
+      }
+      if (status === 'playing') {
+        actions.pause();
+      } else {
+        actions.resume();
+      }
+    }
+  }
+  return actions;
 }
 
 function addCountdownDom() {
-  console.log('add count down dome--->');
   const countdownDom = document.createElement('div');
   countdownDom.classList.add('ti-countdown');
   countdownDom.classList.add('readerControls_item');
   countdownDom.innerHTML = `
-  <div class="ti-countdown-number"></div>
-  <svg class="ti-countdown-svg">
-    <circle class="ti-countdown-circle" r="22" cx="24" cy="24"></circle>
-  </svg>
+    <div class="ti-countdown-number"></div>
+    <svg class="ti-countdown-svg">
+      <circle class="ti-countdown-circle" r="22" cx="24" cy="24"></circle>
+    </svg>
   `
   if (document.querySelector('.readerControls')) {
     document.querySelector('.readerControls').appendChild(countdownDom);
-    const reset = countdown(countdownDom, 15);
+    const countdownAction = countdown(countdownDom, 15);
 
     countdownDom.addEventListener('click', () => {
-       reset();
-       countdown(countdownDom, 15);
+      countdownAction.toggle();
     })
   }
 }
